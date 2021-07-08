7a348a1c8f6ea18b7f73fc96b37feeb54ffca204
Driki520 <87110633+Driki520@users.noreply.github.com>
2021-07-08 12:24:19 +0700

    Add files via upload

---
+++ jajar genjang bolong.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,k,n;
+	
+	printf("Masukan kolom: ");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--){
+		for(j=1;j<=i-1;j++){
+			printf(" ");
+		}
+			for(k=1;k<=n;k++){
+				if(i==1||i==n||k==1||k==n){
+				printf("*");
+			}	else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
