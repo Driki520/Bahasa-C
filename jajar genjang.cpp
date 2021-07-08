7deb140cdfd9a8021c979c9a123b94a78c52645a
Driki520 <87110633+Driki520@users.noreply.github.com>
2021-07-08 12:32:43 +0700

    Add files via upload

---
+++ jajar genjang.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,n,k;
+	printf("Masukan jumlah kolom: ");
+	scanf("%d",&n);
+	
+	for(i=n;i>=1;i--){
+		for(j=1;j<=i-1;j++){
+			printf(" ");
+		}
+		for(k=1;k<=n;k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
