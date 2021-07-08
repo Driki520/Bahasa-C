d36c7c746a973b911bdd5e80424b18d9461084c6
Driki520 <87110633+Driki520@users.noreply.github.com>
2021-07-08 12:26:48 +0700

    Add files via upload

---
+++ Daftar harga toko.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+	int p;
+		
+	printf("\t\t-----------Daftar harga------------\n");
+	printf("1.Kopi\n");
+	printf("2.Teh\n");
+	printf("3.susu\n");
+	printf("4.gorengan\n");
+	z
+	printf("Masukan pilihan: ");
+	scanf("%d",&p);
+	if(p==1){
+		printf("Rp.3000");
+	}
+	else if(p==2){
+		printf("Rp.2000");
+	}
+	else if(p==3){
+		printf("Rp.3000");
+	}
+	else if(p==4){
+		printf("Rp.1000");
+	}
+	else{
+		printf("Data yang dimasukan salah");
+	}
+	return 0;
+}
